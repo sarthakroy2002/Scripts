da8cfe3072b95c9526c243516ad1aae1016b831b
Sarthak Roy <sarthakroy2002@gmail.com>
2021-05-23 20:12:29 +0530

    Added exports

--- Kernel/clang-9.sh
+++ Kernel/clang-9.sh
@@ -2,7 +2,9 @@
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
-
+export ARCH=arm64
+export KBUILD_BUILD_HOST=titan
+export KBUILD_BUILD_USER="sarthakroy2002"
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
