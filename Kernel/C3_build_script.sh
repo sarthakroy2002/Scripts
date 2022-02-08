60333bf3344879c827f0a0786ff2e994298afd54
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-08 20:01:36 +0530

    Fix up my Build script for CI builds and Bot integration
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I642ee81ddafc6c45bae1cab213aadba80c8f12b3

--- Kernel/C3_build_script.sh
+++ Kernel/C3_build_script.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
 rm -rf AnyKernel
-git clone --depth=1 https://github.com/sarthakroy2002/clang-r353983c1 clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
+git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
 KERNEL_DIR=$(pwd)
-PATH="${PWD}/clang/bin:$PATH"
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}"
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 export ARCH=arm64
-export KBUILD_BUILD_HOST=titan
-export KBUILD_BUILD_USER="iamlordutkarsh"
+export KBUILD_BUILD_HOST=neolit
+export KBUILD_BUILD_USER="sarthakroy2002"
+source ~/.bashrc && source ~/.profile
+ccache -M 100G
+export LC_ALL=C && export USE_CCACHE=1
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
@@ -26,7 +29,7 @@ function sendinfo() {
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• Test Kernel •</b>%0ABuild started on <code>Titan</code>%0AFor device <b>Realme C3</b> (RMX2020)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
+        -d text="<b>• Test Kernel •</b>%0ABuild started on <code>Server</code>%0AFor device <b>Realme C3</b> (RMX2020)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
 }
 # Push kernel to channel
 function push() {
@@ -49,12 +52,21 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-    make O=out ARCH=arm64 rmx2020_defconfig
-    make -j$(nproc --all) O=out \
-                          ARCH=arm64 \
-			  CC=clang \
-			  CROSS_COMPILE=aarch64-linux-gnu- \
-			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+[ -d "out" ] && rm -rf out || mkdir -p out
+make O=out ARCH=arm64 RMX2020_defconfig
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
+                      LD=ld.lld \
+                      AS=llvm-as \
+		              AR=llvm-ar \
+			          NM=llvm-nm \
+			          OBJCOPY=llvm-objcopy \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
 
     if ! [ -a "$IMAGE" ]; then
         finerr
@@ -65,7 +77,9 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Test-OSS-KERNEL-RMX2020-${TANGGAL}.zip *
+    zip -r9 NEOLIT-Test-OSS-KERNEL-RMX2020-${TANGGAL}.zip *
+    curl -sL https://git.io/file-transfer | sh
+    ./transfer wet Test-OSS-KERNEL-RMX2020-NEOLIT.zip
     cd ..
 }
 sendinfo
