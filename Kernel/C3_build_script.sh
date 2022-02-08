cd4d8c68c7fd3cbe66b9f806c954ccf293018643
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-08 21:09:59 +0530

    Add my Anykernel repo

--- Kernel/C3_build_script.sh
+++ Kernel/C3_build_script.sh
@@ -72,6 +72,7 @@ make -j$(nproc --all) O=out \
         finerr
         exit 1
     fi
+    git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
