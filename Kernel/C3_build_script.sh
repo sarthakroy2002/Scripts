d23be04ada9a78cc9c6a574a621a5c70ec12160e
Sarthak Roy <sarthakroy2002@gmail.com>
2022-03-11 12:44:42 +0530

    Switch to Clang 14.0.0 for Q kernel

--- Kernel/C3_build_script.sh
+++ Kernel/C3_build_script.sh
@@ -8,7 +8,7 @@ deps() {
 	if [ "${BRANCH}" = "R" ];then
 	    git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 	else
-	    git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-7612306 clang
+	    git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-r437112 clang
     
 	    if [ ! -d "los-4.9-64" ];then
 		git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
