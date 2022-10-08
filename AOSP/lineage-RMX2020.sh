6de06ee92c39aa97edd9ef2912bda403e57703cd
Deep Roy <sarthakroy2002@outlook.com>
2022-10-08 15:18:52 +0530

    Update for lineage-20.0

--- AOSP/lineage-RMX2020.sh
+++ AOSP/lineage-RMX2020.sh
@@ -8,7 +8,7 @@ rm -rf "${SOURCEDIR}"
 mkdir -p "${SOURCEDIR}"
 cd "${SOURCEDIR}"
 
-repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-19.1
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-20.0
 repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
 
 cd frameworks/base
@@ -26,25 +26,17 @@ git fetch https://review.arrowos.net/ArrowOS/android_frameworks_av refs/changes/
 git cherry-pick FETCH_HEAD
 cd "${SOURCEDIR}"
 
-cd system/core
-git fetch https://review.arrowos.net/ArrowOS/android_system_core refs/changes/00/16200/1
-git cherry-pick FETCH_HEAD
-cd "${SOURCEDIR}"
+cd system/core && git fetch https://review.arrowos.net/ArrowOS/android_system_core refs/changes/93/17693/1 && git cherry-pick FETCH_HEAD && cd ../..
 
-cd frameworks/opt/net/ims
-git fetch https://github.com/AOSP-12-RMX2020/frameworks_opt_net_ims
-git cherry-pick 4f35ccb8bf0362c31bf5f074bcb7070da660412a^..3fe1cb7b6b2673adfce2b9232dfaf81375398efb
-cd "${SOURCEDIR}"
+cd frameworks/opt/net/ims && git fetch https://github.com/AOSP-13-RMX2020/frameworks_opt_net_ims && git cherry-pick 0e8a88ecdbf05509f84bd136c63d56791dbf78c3^..5c6179402cd8a5fc29dbd789e59a5ddf5546c1b3 && cd ../../../.. 
 
-cd packages/modules/Wifi
-git fetch https://github.com/AOSP-12-RMX2020/packages_modules_Wifi
-git cherry-pick c6e404695bc451a9667f4893501ef8fe78e1a0b7^..90fc3f6781171dc27fed16b60575f9ea62f02e7a
-cd "${SOURCEDIR}"
+cd packages/modules/Wifi && git fetch https://github.com/AOSP-13-RMX2020/packages_modules_Wifi && git cherry-pick 1315ccb757bd2d7c63b4815ab77e04535d2b7750^..6b341eefeb1127a97dc3b77a853e30ed7630be30 && cd ../../.. 
 
-cd frameworks/opt/telephony
-git fetch https://github.com/phhusson/platform_frameworks_opt_telephony android-12.0.0_r26-phh
-git cherry-pick 6f116d4cdb716072261ecfe532da527182f6dad6
-cd "${SOURCEDIR}"
+cd frameworks/opt/telephony && git fetch https://github.com/AOSP-13-RMX2020/frameworks_opt_telephony && git cherry-pick 3d7ef06b1370b98fc9893693a23b2f350a8d912d && cd ../../..
+
+cd frameworks/base && git fetch https://github.com/ArrowOS/android_frameworks_base arrow-13.0 && git cherry-pick f627e89f23690ebf10ee46a0a3cdc456562ccb02 && git cherry-pick f5ef95dbb73ce6d0167dd085cbca11049919b8a4 && cd ../..
+
+cd packages/modules/Bluetooth && git fetch https://review.arrowos.net/ArrowOS/android_packages_modules_Bluetooth refs/changes/43/18143/1 && git cherry-pick FETCH_HEAD && git fetch https://review.arrowos.net/ArrowOS/android_packages_modules_Bluetooth refs/changes/44/18144/1 && git cherry-pick FETCH_HEAD && cd ../../..
 
 cd vendor/lineage
 wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/0001-Device-Specific-patches.patch
@@ -54,8 +46,9 @@ cd "${SOURCEDIR}"
 git clone --depth=1 https://github.com/sarthakroy2002/vendor_realme_RMX2020.git vendor/realme/RMX2020
 git clone --depth=1 https://github.com/sarthakroy2002/device_realme_RMX2020.git device/realme/RMX2020
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_realme_RMX2020 kernel/realme/RMX2020
-git clone --depth=1 https://github.com/sarthakroy2002/vendor_realme_RMX2020-ims -b twelve-rmui1 vendor/realme/RMX2020-ims
-git clone --depth=1 https://github.com/ArrowOS/android_device_mediatek_sepolicy_vndr -b arrow-12.1 device/mediatek/sepolicy_vndr
+git clone --depth=1 https://github.com/sarthakroy2002/vendor_realme_RMX2020-ims vendor/realme/RMX2020-ims
+git clone --depth=1 https://github.com/sarthakroy2002/android_device_mediatek_sepolicy_vndr -b arrow-13.0-RMX2020 device/mediatek/sepolicy_vndr
+git clone https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-r437112 prebuilts/clang/host/linux-x86/clang-r437112
 
 cd device/realme/RMX2020
 mv arrow_RMX2020.mk lineage_RMX2020.mk
