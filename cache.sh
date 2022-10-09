fc358a4442ea8a678962f39f74ca4e265c665b3d
Abhinav Gupta <abhinavgupta371@gmail.com>
2022-10-09 13:26:02 +0530

    Create cache.sh

---
+++ cache.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd /tmp || exit 1
+git clone https://github.com/ccache/ccache.git
+cd ccache || exit 1
+./autogen.sh
+./configure --disable-man --with-libzstd-from-internet --with-libb2-from-internet
+make -j"$(nproc)"
+sudo make install
+rm -rf "${PWD}"
+cd - || exit 1
+ 
