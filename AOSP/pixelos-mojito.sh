cd32cde2fa95ded0146013af00984dded1fa00f5
Ashwin DS <astroashwin@outlook.com>
2022-10-09 10:25:29 +0400

    add script for PixelOS mojito

---
+++ AOSP/pixelos-mojito.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+rm -rf /cache/*
+
+BASE_DIR="$(pwd)"
+SOURCEDIR="${BASE_DIR}/work"
+
+git config --global user.email "sarthakroy2002@gmail.com" && git config --global user.name "Sarthak Roy"
+df -h
+mkdir -p "${SOURCEDIR}"
+cd "${SOURCEDIR}"
+
+repo init -u https://github.com/PixelOS-Pixelish/manifest.git -b thirteen --depth=1
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
+
+
+git clone --depth=1 https://github.com/PixelOS-Devices/device_xiaomi_mojito device/xiaomi/mojito
+git clone --depth=1 https://github.com/PixelOS-Devices/vendor_xiaomi_mojito vendor/xiaomi/mojito
+git clone --depth=1 https://github.com/PixelOS-Devices/kernel_xiaomi_mojito kernel/xiaomi/mojito
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_hardware_xiaomi hardware/xiaomi
+
+source build/envsetup.sh
+lunch aosp_mojito-userdebug
+
+mka bacon
+
+cd out/target/product/mojito
+curl -sL https://git.io/file-transfer | sh
+./transfer wet Pixel*.zip
+
+exit 0
