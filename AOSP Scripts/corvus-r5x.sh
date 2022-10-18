4d94cc519884ce4524c3f1638a19042c89facd54
madmiz <mizdrake7@gmail.com>
2022-10-18 14:24:10 +0000

    corvus_r5x-script: Use Brightness Curve Patch From ArrowOS
    * Without it brigtness scale and control is derped. Credits to @SarthakRoy2022 for the patch
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- AOSP Scripts/corvus-r5x.sh
+++ AOSP Scripts/corvus-r5x.sh
@@ -14,6 +14,12 @@ cd "${SOURCEDIR}"
 repo init --depth=1 -u https://github.com/Corvus-AOSP/android_manifest.git -b 13
 repo sync  --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)
 
+# Source Patches (Optional)
+cd frameworks/base
+wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices -a12l.patch
+patch -p1 <*.patch
+cd "${SOURCEDIR}"
+
 # Sync Trees
 git clone https://github.com/CorvusRom-Devices/device_realme_r5x -b 13-wip device/realme/r5x
 git clone https://github.com/CorvusRom-Devices/vendor_realme_r5x vendor/realme/r5x
