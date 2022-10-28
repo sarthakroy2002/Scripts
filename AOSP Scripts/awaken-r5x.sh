035be64d31d475ede56cfe010267ccc2ca9c37db
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-10-29 00:37:08 +0530

    Added my awaken build script for r5x

---
+++ AOSP Scripts/awaken-r5x.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+rm -rf /cache/*
+
+BASE_DIR="$(pwd)"
+SOURCEDIR="${BASE_DIR}/awakenn"
+
+git config --global user.email "mishrabiswajit660@gmail.com" && git config --global user.name "COSMOS"
+df -h
+mkdir -p "${SOURCEDIR}"
+cd "${SOURCEDIR}"
+
+# Sync Source
+repo init -u https://github.com/Project-Awaken/android_manifest -b 12.1
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+
+# Source Patches (Optional)
+cd frameworks/base
+wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices -a12l.patch
+patch -p1 <*.patch
+cd "${SOURCEDIR}"
+
+# Sync Trees
+git clone https://github.com/mishrabiswajit/device_realme_r5x -b 13-wip device/realme/r5x
+git clone https://github.com/mishrabiswajit/vendor_realme_r5x vendor/realme/r5x
+git clone https://github.com/mishrabiswajit/kernel_realme_r5x kernel/realme/r5x
+
+# Start Build
+source build/envsetup.sh
+lunch awaken_r5x-userdebug
+make bacon -j$(nproc --all)
+
+# Upload
+cd out/target/product/r5x
+curl -sL https://git.io/file-transfer | sh
+./transfer wet Awaken*.zip
