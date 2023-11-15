9421dd34e911baa0926fb9b503cbb730c3f593df
Sarthak Roy <sarthakroy2002@gmail.com>
2023-11-15 23:38:33 +0530

    C3_build_script: Refactor!
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: If8605b236edd1cae9ed2d1ff6fdd81824de00cd6

--- Kernel/C3_build_script.sh
+++ Kernel/C3_build_script.sh
@@ -3,26 +3,11 @@
 # Dependencies
 deps() {
     echo "Cloning dependencies"
-
     if [ ! -d "clang" ]; then
-        if [ "${BRANCH}" = "R" ]; then
-            wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-14.0.0_r2/clang-r487747c.tar.gz -O "aosp-clang.tar.gz"
-            mkdir clang && tar -xf aosp-clang.tar.gz -C clang && rm -rf aosp-clang.tar.gz
-            KBUILD_COMPILER_STRING="Clang 17.0.2 r487747c"
-            PATH="${PWD}/clang/bin:${PATH}"
-        else
-            git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b clang
-            KBUILD_COMPILER_STRING="Clang 14.0.1 r437112b"
-
-            if [ ! -d "los-4.9-64" ]; then
-                git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-            fi
-
-            if [ ! -d "los-4.9-32" ]; then
-                git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
-            fi
-            PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}"
-        fi
+        wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-14.0.0_r2/clang-r487747c.tar.gz -O "aosp-clang.tar.gz"
+        mkdir clang && tar -xf aosp-clang.tar.gz -C clang && rm -rf aosp-clang.tar.gz
+        KBUILD_COMPILER_STRING="Clang 17.0.2 r487747c"
+        PATH="${PWD}/clang/bin:${PATH}"
     fi
     sudo apt install -y ccache
     echo "Done"
@@ -73,13 +58,14 @@ tgs() {
         -F "caption=$2 | *MD5*: \`$MD5\`"
 }
 
-# sticker plox
+# Sticker
 sticker() {
     curl -s -X POST https://api.telegram.org/bot"${token}"/sendSticker \
-        -d sticker="CAACAgQAAxkBAAED3JFiApkFOuZg8zt0-WNrfEGwrvoRuAACAQoAAoEcoFINevKyLXEDhSME" \
+        -d sticker="CAACAgUAAxkBAAEKwfdlVQiHDyeU33wu71hiEWMeemBHhAACTAUAAgjXqFdZ0rpx15brLTME" \
         -d chat_id="${chat_id}"
 }
-# Send info plox channel
+
+# Send Build Info
 sendinfo() {
     tg "
 • neOliT CI Build •
@@ -90,14 +76,17 @@ sendinfo() {
 *Last Commit*: [${COMMIT_HASH}](${REPO}/commit/${COMMIT_HASH})
 *Compiler*: \`${KBUILD_COMPILER_STRING}\`
 *Build Status*: \`${STATUS}\`"
+    sticker
 }
+
 # Push kernel to channel
 push() {
     cd AnyKernel || exit 1
     ZIP=$(echo *.zip)
     tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For *${DEVICE} (${CODENAME})* | ${KBUILD_COMPILER_STRING}"
 }
-# Find Error
+
+# Catch Error
 finderr() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
         -d chat_id="$chat_id" \
@@ -107,7 +96,8 @@ finderr() {
         -d text="Build throw an error(s)"
     exit 1
 }
-# Compile plox
+
+# Compile
 compile() {
 
     if [ -d "out" ]; then
@@ -115,40 +105,25 @@ compile() {
     fi
 
     make O=out ARCH="${ARCH}" "${DEFCONFIG}"
-
-    if [ "${BRANCH}" = "R" ]; then
-        make -j"${PROCS}" O=out \
-            ARCH=$ARCH \
-            CC="clang" \
-            LLVM=1 \
-            LLVM_IAS=1 \
-            CONFIG_NO_ERROR_ON_MISMATCH=y
-    else
-        make -j"${PROCS}" O=out \
-            ARCH=$ARCH \
-            CC="clang" \
-            CLANG_TRIPLE=aarch64-linux-gnu- \
-            CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
-            CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
-            CONFIG_NO_ERROR_ON_MISMATCH=y
-    fi
+    make -j"${PROCS}" O=out \
+        ARCH=$ARCH \
+        CC="clang" \
+        LLVM=1 \
+        LLVM_IAS=1 \
+        CONFIG_NO_ERROR_ON_MISMATCH=y
 
     if ! [ -a "$IMAGE" ]; then
         finderr
         exit 1
     fi
 
-    if [ "${BRANCH}" = "arrow-12.1" ] || [ "${BRANCH}" = "arrow-12.0" ]; then
-        git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git -b RMX2020-12 AnyKernel
-    else
-        git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
-    fi
+    git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 neOliT-Test-OSS-"${BRANCH}"-KERNEL-"${CODENAME}"-"${DATE}".zip ./*
+    zip -r9 neOliT-Test-OSS-"${BRANCH}"-RUI2-KERNEL-"${CODENAME}"-"${DATE}".zip ./*
     cd ..
 }
 
