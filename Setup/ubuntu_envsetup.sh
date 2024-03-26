4d556ad972b2198126838364a1b60dd9206ae837
Anicetus <aniketkundu12072004@gmail.com>
2024-03-26 20:56:48 +0530

    Scripts: Add git-lfs for installing during setup (#87)
    - It is needed for large storage file support

--- Setup/ubuntu_envsetup.sh
+++ Setup/ubuntu_envsetup.sh
@@ -6,7 +6,7 @@ echo 'Lets start Setup'
 # Install Essential stuff
 sudo apt update
 sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-sudo apt install -y python tmate ssh python2 patchelf binutils python-is-python3 zram-config
+sudo apt install -y python tmate ssh python2 patchelf binutils python-is-python3 zram-config git-lfs
 echo 'Essential stuff are installed successfully'
 
 # My Git Config
