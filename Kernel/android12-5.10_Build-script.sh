0957202577eef638c5cb34e5b395400161b68ece
Sarthak Roy <sarthakroy2002@gmail.com>
2024-07-07 22:02:58 +0530

    Kernel: Add android12-5.10 Build script
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I089be13ae47b4b660692a8e14e2f78ad5b781e38

---
+++ Kernel/android12-5.10_Build-script.sh
@@ -0,0 +1,17 @@
+mkdir kernel
+cd kernel
+repo init -u https://android.googlesource.com/kernel/manifest.git -b common-android12-5.10-lts --depth=1
+repo sync --force-sync
+ln -s common/build.config.gki.aarch64 build.config
+cd common
+sed -i "s|Image.lz4|Image.gz|g" build.config.gki.aarch64
+wget https://gist.githubusercontent.com/sarthakroy2002/4e1c6bc5b5745168f1d47f4eb1bb25d6/raw/1a70b2aa9c401262de2dad03f8231b3f03d5325d/uncommitted.patch
+patch -p1 < uncommitted.patch
+cd ..
+echo "Compile starting..."
+bash build/build.sh
+echo "Compilied..."
+git clone https://github.com/sarthakroy2002/AnyKernel3 -b android12-5.10 
+cp -r out/android12-5.10/dist/Image.gz AnyKernel3
+cd AnyKernel3
+zip -r9 android12-5.10-AnyKernel3.zip ./*
