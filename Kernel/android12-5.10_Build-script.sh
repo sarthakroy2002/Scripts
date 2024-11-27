e1112229fb98da3d722110474825419c4b3b2ee0
Sarthak Roy <sarthakroy2002@gmail.com>
2024-11-28 05:20:28 +0530

    Kernel: Drop uvc patchsets

--- Kernel/android12-5.10_Build-script.sh
+++ Kernel/android12-5.10_Build-script.sh
@@ -4,8 +4,6 @@ repo init -u https://android.googlesource.com/kernel/manifest.git -b common-andr
 repo sync --force-sync
 ln -s common/build.config.gki.aarch64 build.config
 cd common
-git fetch https://github.com/PQEnablers-Devices/android_kernel_mediatek_5.10 690af439252ed79b1ecfd41cac7c95cf14e81c5a
-git cherry-pick 88387bb803d881312cc24d97b680ccf8bcabd4de^..690af439252ed79b1ecfd41cac7c95cf14e81c5a
 sed -i "s|Image.lz4|Image.gz|g" build.config.gki.aarch64
 wget https://gist.githubusercontent.com/sarthakroy2002/4e1c6bc5b5745168f1d47f4eb1bb25d6/raw/1a70b2aa9c401262de2dad03f8231b3f03d5325d/uncommitted.patch
 patch -p1 < uncommitted.patch
